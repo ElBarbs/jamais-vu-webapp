f044d0b3336bf4a842222e3b26c4320d99e7b865
Louis B. <47404526+ElBarbs@users.noreply.github.com>
2024-11-11 19:38:48 -0500

    Removed normalization.

--- src/components/waveform.tsx
+++ src/components/waveform.tsx
@@ -16,7 +16,6 @@ export default function Waveform({ url, playing = false }: WaveformProps) {
         container: containerRef.current,
         barWidth: 2,
         sampleRate: 48000,
-        normalize: true,
       });
 
       wavesurferRef.current.on("finish", () => {
